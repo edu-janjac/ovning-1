33d409c6a75fa4ca762202d3bcafdf30e73ee6c6
edu-jacjan <jacob.janssonfarnlof@skola.taby.se>
2025-11-06 13:04:41 +0100

    del1

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ovning-1
+
+def get_name() skaffar namn från användare
+
+def main() är huvudkoden som anropar get_name() för att sen printa ut strängen med namnet
---
+++ del1.py
@@ -0,0 +1,9 @@
+def get_name():
+    name = input("Namn?: ")
+    return name
+
+def main():
+    name = get_name()
+    print(f"Hej {name} och välkomenen till the squid games!")
+
+main()
